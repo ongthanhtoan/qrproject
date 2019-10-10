1ccfbfb1e75595ef4310e44d0676060ad75cad77
ongthanhtoan <44467058+ongthanhtoan@users.noreply.github.com>
2019-10-10 12:24:13 +0700

    a

--- app/Http/Controllers/KiemKeController.php
+++ app/Http/Controllers/KiemKeController.php
@@ -13,6 +13,7 @@ class KiemKeController extends Controller
 		->join('loai','taisan_'.$Year.'.l_MaLoai','=','loai.l_MaLoai')
 		->join('hientrang','taisan_'.$Year.'.ht_MaHT','=','hientrang.ht_MaHT')
 		->join('canbo','taisan_'.$Year.'.cb_TenDangNhap','=', 'canbo.cb_TenDangNhap')
+		->where('ts_MaTS',$id)
 		->get();
 		return view('backend.taisan.show')->with('danhsachtaisan',$TaiSan);
 	}
